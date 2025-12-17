bc9bcaf846a9edc55b219576a4bdf6062fc6e922
Dhruv Patni <patnidhruv13@gamil.com>
2025-12-17 12:58:51 +0530

    auth.php file added

---
+++ includes/auth.php
@@ -0,0 +1,22 @@
+<?php   
+if(session_status() == PHP_SESSION_NONE){
+session_start();
+}
+
+function requireLogin(){
+    if(!isset($_SESSION['user'])){
+        header('Location: /admin/login.php');
+        exit();
+    }
+}
+
+function requireRole($roles = []){
+    requireLogin();
+
+    if(!in_array($_SESSION['user']['role_name'], $roles)){
+        http_response_code(403);
+        echo "access denied.";
+        exit();
+    }
+}
+?>
